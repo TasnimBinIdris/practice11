b11bb2cc911e64c3041b51e5f45f450eec84ebb5
TasnimBinIdris <smidris047@gmail.com>
2021-04-15 16:29:11 +0600

    first commit

---
+++ src/components/Home/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../../Shared/Navbar/Navbar';
+import Header from '../Header/Header';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>home</h1>
+            <Navbar></Navbar>
+            <Header></Header>
+        </div>
+    );
+};
+
+export default Home;
